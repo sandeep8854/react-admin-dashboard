be4d879c1d37a8226fa3ad59e9cbe4d99808b465
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2023-11-01 13:02:21 +0530

    modified top bar section

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import { Language, Settings } from "@mui/icons-material";
+import "./topbar.css";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Sandeep</span>
+        </div>
+        <div className="topright">
+          <div className="topbarIconContainer">
+            <NotificationsNoneIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/725463/pexels-photo-725463.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Avatar not found"
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
