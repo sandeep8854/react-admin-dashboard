d97c3ad19f60088cd17d617da6641645d4f63a8b
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2023-11-01 13:15:56 +0530

    adding dummay data

---
+++ src/dummyData.js
@@ -0,0 +1,240 @@
+export const data = [
+  {
+    name: "Jan",
+    "Active User": 3000,
+  },
+  {
+    name: "Feb",
+    "Active User": 1800,
+  },
+  {
+    name: "Mar",
+    "Active User": 2200,
+  },
+  {
+    name: "Apr",
+    "Active User": 3700,
+  },
+  {
+    name: "May",
+    "Active User": 3400,
+  },
+  {
+    name: "Jun",
+    "Active User": 3900,
+  },
+  {
+    name: "Jul",
+    "Active User": 4000,
+  },
+  {
+    name: "Aug",
+    "Active User": 7400,
+  },
+  {
+    name: "Sep",
+    "Active User": 8700,
+  },
+  {
+    name: "Oct",
+    "Active User": 1100,
+  },
+  {
+    name: "Nov",
+    "Active User": 2700,
+  },
+  {
+    name: "Dec",
+    "Active User": 7400,
+  },
+];
+
+export const rowsUser = [
+  {
+    id: 1,
+    username: "Madhuri Raughela",
+    avatar:
+      "https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=400",
+    email: "madhuri@gmail.com",
+    status: "active",
+    transaction: "₹ 9060",
+  },
+  {
+    id: 2,
+    username: "Madhuri Raughela",
+    avatar:
+      "https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=400",
+    email: "madhuri@gmail.com",
+    status: "active",
+    transaction: "₹ 9060",
+  },
+  {
+    id: 3,
+    username: "Madhuri Raughela",
+    avatar:
+      "https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=400",
+    email: "madhuri@gmail.com",
+    status: "active",
+    transaction: "₹ 9060",
+  },
+  {
+    id: 4,
+    username: "Madhuri Raughela",
+    avatar:
+      "https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=400",
+    email: "madhuri@gmail.com",
+    status: "active",
+    transaction: "₹ 9060",
+  },
+  {
+    id: 5,
+    username: "Madhuri Raughela",
+    avatar:
+      "https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=400",
+    email: "madhuri@gmail.com",
+    status: "active",
+    transaction: "₹ 9060",
+  },
+  {
+    id: 6,
+    username: "Madhuri Raughela",
+    avatar:
+      "https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=400",
+    email: "madhuri@gmail.com",
+    status: "active",
+    transaction: "₹ 9060",
+  },
+  {
+    id: 7,
+    username: "Madhuri Raughela",
+    avatar:
+      "https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=400",
+    email: "madhuri@gmail.com",
+    status: "active",
+    transaction: "₹ 9060",
+  },
+  {
+    id: 8,
+    username: "Madhuri Raughela",
+    avatar:
+      "https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=400",
+    email: "madhuri@gmail.com",
+    status: "active",
+    transaction: "₹ 9060",
+  },
+  {
+    id: 9,
+    username: "Madhuri Raughela",
+    avatar:
+      "https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=400",
+    email: "madhuri@gmail.com",
+    status: "active",
+    transaction: "₹ 9060",
+  },
+  {
+    id: 10,
+    username: "Madhuri Raughela",
+    avatar:
+      "https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=400",
+    email: "madhuri@gmail.com",
+    status: "active",
+    transaction: "₹ 9060",
+  },
+];
+export const productData = [
+  {
+    name: "Jan",
+    Sales: 4000,
+  },
+  {
+    name: "Feb",
+    Sales: 3000,
+  },
+  {
+    name: "Mar",
+    Sales: 5000,
+  },
+];
+
+export const productRows = [
+  {
+    id: 1,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "₹ 9060",
+  },
+  {
+    id: 2,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "₹ 9060",
+  },
+  {
+    id: 3,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "₹ 9060",
+  },
+  {
+    id: 4,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "₹ 9060",
+  },
+  {
+    id: 5,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "₹ 9060",
+  },
+  {
+    id: 6,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "₹ 9060",
+  },
+  {
+    id: 7,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "₹ 9060",
+  },
+  {
+    id: 8,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "₹ 9060",
+  },
+  {
+    id: 9,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "₹ 9060",
+  },
+  {
+    id: 10,
+    name: "Apple Airpods",
+    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    stock: 123,
+    status: "active",
+    price: "₹ 9060",
+  },
+];
